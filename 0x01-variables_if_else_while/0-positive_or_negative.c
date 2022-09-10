2685341c36feef350bada2604e83db12e0b124a3
mycredible <mycredible@users.noreply.github.com>
2022-09-10 08:21:13 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  *
- * Description:points if a number is positive or negative
+ * Description: points if a number is positive or negative
  * Return: 0
  */
 int main(void)
@@ -13,23 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if (n > 0);
-
-	continue;
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-
 	else if (n == 0)
 	{
-	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
-
 	else if (n < 0)
 	{
-	printf("%d is negative\n", n);
-
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
-
