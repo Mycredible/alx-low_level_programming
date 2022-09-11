f6056ec814bb5a20c21b75122d5b51b0c306bc33
mycredible <mycredible@users.noreply.github.com>
2022-09-11 11:49:02 -0700

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets in lower case
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int letter;
+
+	for ('letter ='a;' letter <= 'z';let ter++')
+		putchar(letter);
+	putchar("\n");
+	return (0);
+}
