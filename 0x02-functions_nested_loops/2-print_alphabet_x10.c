356837c4497a859caf66d3212718e619603a09e6
mycredible <mycredible@users.noreply.github.com>
2022-09-14 03:21:50 -0700

    task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet.
+ ^
+ ^
+ */
+
+void print_alphabet_x10(void)
+
+
+{
+	char me;
+	int = i;
+
+	i = 0;
+
+	while (i < 10);
+	{
+		me = 'a';
+		while (me <= 'z');
+		{
+			_putchar(me);
+			me++;
+		}
+		_putchar('\n');
+		i++;
+
+	}
+}
