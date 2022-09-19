5d2c7d5d847bc57b071d21cff1b99c95108e78f4
mycredible <mycredible@users.noreply.github.com>
2022-09-19 08:22:37 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - pointer to an int as parameter and update it points to 98.
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this project is on array, string and pointer
